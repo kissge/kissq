c04b9a39ec610a5f55847c7a5adf42c505ddca41
Yusuke Kido <mail@yusuk.eki.do>
2025-10-09 22:28:08 +0900

    Refactor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,128 +5,131 @@
 {@render children?.()}
 
 <style>
-	:global(html, body) {
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-		margin: 0;
-		padding: 0;
-		min-height: 100dvh;
-		overflow-x: hidden;
-		font-family: sans-serif;
-	}
-
-	:global(button) {
-		border-radius: 0.3em;
-		padding: 0.2em 0.5em;
-		font-size: 1.2rem;
-		user-select: none;
-
-		:global(&:not(:disabled)) {
-			cursor: pointer;
-
-			&:hover,
-			&:focus {
-				box-shadow: 3px 3px 5px 3px #ccc;
-			}
-
-			&:active {
-				box-shadow: inset 3px 3px 5px 3px #ccc;
-			}
-		}
-	}
-
-	:global(label:has([disabled])) {
-		color: #aaa;
-	}
-
-	:global(label:not(:has([disabled]))) {
-		cursor: pointer;
-		border-radius: 0.5em;
-
-		&:hover {
-			background-color: #eee;
-		}
-	}
-
-	:global(.crossfade[inert]) {
-		/** アニメーションががたがたしないように */
-		display: inline-block;
-		width: 0;
-		height: 0;
-	}
-
-	:global(dialog) {
-		cursor: default;
-		box-sizing: border-box;
-		box-shadow: 8px 8px 10px 0 #444;
-		border-radius: 0.5em;
-		width: min(90%, 800px);
-		max-height: 90dvh;
-		font-size: 1.5rem;
-		user-select: none;
-
-		:global(.buttons) {
+	:global {
+		html,
+		body {
 			display: flex;
-			justify-content: end;
-			gap: 0.5em;
+			flex-direction: column;
+			justify-content: space-between;
+			margin: 0;
+			padding: 0;
+			min-height: 100dvh;
+			overflow-x: hidden;
+			font-family: sans-serif;
+		}
 
-			:global(.primary:not([disabled])) {
-				background-color: #06f;
-				color: white;
+		button {
+			border-radius: 0.3em;
+			padding: 0.2em 0.5em;
+			font-size: 1.2rem;
+			user-select: none;
+
+			&:not(:disabled) {
+				cursor: pointer;
+
+				&:hover,
+				&:focus {
+					box-shadow: 3px 3px 5px 3px #ccc;
+				}
+
+				&:active {
+					box-shadow: inset 3px 3px 5px 3px #ccc;
+				}
 			}
 		}
 
-		:global(.table) {
-			display: grid;
-			grid-template-columns: auto 1fr;
-			gap: 1em 0;
-			margin-bottom: 2em;
+		label:has([disabled]) {
+			color: #aaa;
+		}
 
-			:global(> div) {
-				padding: 1em 0;
+		label:not(:has([disabled])) {
+			cursor: pointer;
+			border-radius: 0.5em;
+
+			&:hover {
+				background-color: #eee;
 			}
+		}
 
-			:global(> :nth-child(2n + 1)) {
+		.crossfade[inert] {
+			/** アニメーションががたがたしないように */
+			display: inline-block;
+			width: 0;
+			height: 0;
+		}
+
+		dialog {
+			cursor: default;
+			box-sizing: border-box;
+			box-shadow: 8px 8px 10px 0 #444;
+			border-radius: 0.5em;
+			width: min(90%, 800px);
+			max-height: 90dvh;
+			font-size: 1.5rem;
+			user-select: none;
+
+			.buttons {
 				display: flex;
 				justify-content: end;
-				align-items: center;
-				border-right: 1px solid #ccc;
-				padding-right: 0.6em;
-				font-weight: bold;
-				font-size: 1rem;
+				gap: 0.5em;
+
+				.primary:not([disabled]) {
+					background-color: #06f;
+					color: white;
+				}
 			}
 
-			:global(> :nth-child(2n + 2)) {
-				padding-left: 0.6em;
-			}
+			.table {
+				display: grid;
+				grid-template-columns: auto 1fr;
+				gap: 1em 0;
+				margin-bottom: 2em;
 
-			:global(> :nth-child(4n + 3)),
-			:global(> :nth-child(4n + 4)) {
-				background-color: #f6f6f6;
-			}
+				> div {
+					padding: 1em 0;
+				}
 
-			:global(input[type='number']),
-			:global(button) {
-				padding: 0 0.5rem;
-				height: 2.5rem;
-			}
+				> :nth-child(2n + 1) {
+					display: flex;
+					justify-content: end;
+					align-items: center;
+					border-right: 1px solid #ccc;
+					padding-right: 0.6em;
+					font-weight: bold;
+					font-size: 1rem;
+				}
 
-			:global(input[type='number']) {
-				width: 3em;
-				font-size: 2rem;
-				text-align: right;
-			}
+				> :nth-child(2n + 2) {
+					padding-left: 0.6em;
+				}
 
-			:global(.error) {
-				display: block;
-				color: red;
-			}
+				> :nth-child(4n + 3),
+				> :nth-child(4n + 4) {
+					background-color: #f6f6f6;
+				}
 
-			:global(hr) {
-				margin: 1.5em 0;
-				border: none;
-				border-top: 1px solid #ccc;
+				input[type='number'],
+				button {
+					padding: 0 0.5rem;
+					height: 2.5rem;
+				}
+
+				input[type='number'] {
+					width: 3em;
+					font-size: 2rem;
+					text-align: right;
+				}
+
+				.error {
+					display: block;
+					color: red;
+				}
+
+				hr {
+					margin: 1.5em 0;
+					border: none;
+					border-top: 1px solid #ccc;
+				}
 			}
 		}
 	}
